e5aea3423d7c1719fd0854bb293d22ac3a89208c
Ryan Rustyn <ryanrustyn@gmail.com>
2022-07-28 12:33:05 -0700

    add redirect for incorrect links, flash for completion

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = [] #list for user responses
+responses = []  # list for user responses
+
 
 @app.get('/')
 def show_home():
     """show home page to begin a survey"""
 
-    return render_template("survey_start.html",
-                            title = survey.title,
-                            instructions = survey.instructions)
+    return render_template("survey_start.html", survey=survey)
+
 
 @app.post('/begin')
 def start_survey():
@@ -24,15 +24,20 @@ def start_survey():
 
     return redirect('/questions/0')
 
-@app.get('/questions/<number>')
+
+@app.get('/questions/<number>')  # if invalid number, redirect
 def show_question(number):
     """shows a question from survey question list"""
 
+    if int(number) > len(responses):
+        number = len(responses)
+        return redirect(f"/questions/{number}")
+
     return render_template("question.html",
-                    question = survey.questions[int(number)])
+                    question=survey.questions[int(number)])
 
 
-@app.post("/answer") #take answer for q1 and redirect to a get for q2
+@app.post("/answer")  # take answer for q1 and redirect to a get for q2
 def log_response_and_redirect():
     """record answer for survey and redirect to next question
         or completion page if complete
@@ -45,11 +50,11 @@ def log_response_and_redirect():
     if next_question_index < len(survey.questions):
         return redirect(f"/questions/{next_question_index}")
     else:
+        flash("Thanks for your input! We appericate you! I love you!")
         return redirect("/complete")
 
 @app.get("/complete")
 def show_completion():
     """show a completion page"""
-
     return render_template("completion.html")
 
